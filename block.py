2bce8d0735b126b1b9e2953a2507200328fb8307
fotinidelig <fotinidelig@gmail.com>
2020-03-24 17:18:19 +0200

    uncommented hash

--- block.py
+++ block.py
@@ -1,24 +1,20 @@
 import blockchain
 import datetime;
 from collections import OrderedDict
+import json
 
 class Block:
-	#def __init__(self, previousHash, nonce):	# to be used once implemented
-	def __init__(self):
+	def __init__(self, **kwargs):
 		##set
 		print('block_init')
-		#self.previousHash=previousHash
+		self.previousHash=kwargs['previousHash']
 		self.timestamp=datetime.datetime.now().timestamp()
-		#self.nonce=nonce
+		self.nonce=kwargs['nonce']
 		self.listOfTransactions=[]
+
 		#calculate self.hash
-		# hash_data = OrderedDict([('prev':self.previousHash),('tmsp':self.timestamp), ('nonce': self.nonce)])
-		# tmp = json.dumps(hash_data) 
-		# self.hash = SHA384.new(tmp.encode())
-	
-	# def myHash():
-	# 	#calculate self.hash
-	# 	print("myHash")
-	# 	hash_data = {'prev':self.previousHash,'tmsp':self.timestamp, 'nonce': self.nonce}
-	# 	tmp = json.dumps(trans) 
- #        self.hash = SHA384.new(tmp.encode())
+		hash_data = OrderedDict([('prev':self.previousHash),('tmsp':self.timestamp), ('nonce': self.nonce)])
+		tmp = json.dumps(hash_data) 
+		self.hash = SHA384.new(tmp.encode())
+
+
