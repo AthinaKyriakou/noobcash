4577cab7eb5ffb51b15707af5ce818aaeeb8aa1b
fotinidelig <fotinidelig@gmail.com>
2020-03-27 20:38:57 +0200

    all requests

--- examples.sh
+++ examples.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
-
-
-# functions='block_br'|'trans_br'|'init'
+#!/bin/bash
 
 function=$1
 
-echo "$(tput setaf 6)<Running: "$function">\n$(tput sgr0)"
+echo "$(tput setaf 6)Enter <help> for options"
+echo "<Running: "$function">$(tput sgr0)"
+
+functions=( "init" "connect" "new_transaction" "view_transactions" "balance" )
+
+expl=('initialize network with bootstrap node'
+		'connect node to network'
+		'create a transaction'
+		"view transactions in last validated block"
+		"view node\'s balance")
+
+use=('use: ./examples.sh init <number of nodes>'
+	'use: ./examples.sh connect <IP> <PORT>'
+	'use ./examples.sh new_transaction <REC_IP>:<REC_PORT> <amount>'
+	'use ./examples.sh view_transactions <PORT>'
+	'use ./examples.sh balance <PORT>')
 
 case $function in
 	init)
@@ -13,22 +25,39 @@ case $function in
 		# use: ./examples.sh init <number of nodes>
 		curl http://localhost:5000/init/$num_of_nodes
 		;;
-	block_br)
-		port=$2
-		# use: ./examples.sh block_br <port>
-		curl -d '{"previousHash":1,"timestamp":2,"nonce":3,"listOfTransactions":4,"blockHash":5}' -H "Content-Type: application/json" -X POST http://localhost:$port/receive_block			
-		;;
-	trans_br)
-		# use: ./examples.sh init trans_br <port>
-		port=$2
-		curl -d '{"sender":1,"receiver":2,"amount":3,"id":4,"transaction_inputs":5,"transaction_outputs":6,"signature":7,"sender_privkey":8}' -H "Content-Type: application/json" -X POST http://localhost:$port/receive_trans
-		;;
 	connect)
 		ip=$2
 		port=$3
-		# use: ./examples.sh connect <IP> <port>
+		# use: ./examples.sh connect <IP> <PORT>
 		curl http://localhost:$port/connect/$ip/$port
+		;;
+	new_transaction)
+		port=$2
+		recipient=$3 # in <IP>:<PORT> format
+		amount=$4
+		data='{"recipient":${recipient},"amount":${amount}}'
+		# use ./examples.sh new_transaction <REC_IP>:<REC_PORT> <amount>
+		curl data -X POST http://localhost:$port/transaction/new
+		;;
+	view_transactions)
+		port=$2
+		# use ./examples.sh view_transactions <PORT>
+		curl http://localhost:$port/transactions/view
+		;;
+	balance)
+		port=$2
+		# use ./examples.sh balance <PORT>
+		curl http://localhost:$port/balance
+		;;
+	help)
+		for i in {0..4}
+		do
+			echo "$(tput setaf 3)"
+			echo "$(tput setaf 6)${functions[$i]}$(tput setaf 3):  ${expl[$i]}"
+			echo "${use[$i]}"
+			echo "$(tput setaf 4)~~~~~~~~~~"
+		done
 		;;
 	*)
 esac
-echo "\n$(tput setaf 6)<Done>$(tput sgr0)"
+echo "$(tput setaf 6)<Done>$(tput sgr0)"
