af41015dbb145a22bd9acb5199a182eaaf7a8c27
Ilia D <ilia.dioleti6@gmail.com>
2020-04-01 18:59:31 +0300

    ignore

--- wallet.py
+++ wallet.py
@@ -21,7 +21,7 @@ class Wallet:
 		rsa_key = RSA.generate(1024)
 		self.private_key = rsa_key.exportKey('PEM').decode()
 		self.public_key = rsa_key.publickey().exportKey('PEM').decode()
-		self.utxos= utxos
+		self.utxos= utxos #key : public key, value: {id, to_who, amount}
 		#self.transactions
 
 	def balance(self):
