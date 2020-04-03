ea1f5a6429fa36a6875594816528e72538a9cedd
Ilia D <ilia.dioleti6@gmail.com>
2020-04-03 21:23:22 +0300

    separate plots for different dfficulties

--- plot_metrics.py
+++ plot_metrics.py
@@ -6,9 +6,19 @@ res5.columns = ["throughput_5", "block_time_5"]
 res10.columns = ["throughput", "block_time"]
 res5["throughput_10"]=res10['throughput']
 res5["block_time_10"]=res10['block_time']
-res5["dif_cap"]=[4.1,4.5,4.10,5.1,5.5,5.10]
+res5["capacity"]=[1,5,10,1,5,10]
+#diffculty 4 (first 3 lines)
+res5.iloc[:3].plot(x="capacity", y=["throughput_5", "throughput_10"], kind="bar")
 f1=plt.figure(1)
-res5.plot(x="dif_cap", y=["throughput_5", "throughput_10"], kind="bar")
+plt.title('throughput for dificulty=4')
+res5.iloc[:3].plot(x="capacity", y=["block_time_5", "block_time_10"], kind="bar")
 f2=plt.figure(2)
-res5.plot(x="dif_cap", y=["block_time_5", "block_time_10"], kind="bar")
+plt.title('Block time for dificulty=4')
+#difficulty 5 (last 3 lines)
+res5.iloc[3:].plot(x="capacity", y=["throughput_5", "throughput_10"], kind="bar")
+f3=plt.figure(3)
+plt.title('throughput for dificulty=5')
+res5.iloc[3:].plot(x="capacity", y=["block_time_5", "block_time_10"], kind="bar")
+f4=plt.figure(4)
+plt.title('Block time for dificulty=5')
 plt.show()
